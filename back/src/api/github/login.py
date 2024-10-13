28594d718b3d0910fbbd9cc4917927c37f669160
aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>
2024-10-13 20:03:55 +0300

    Fix bug with parse response

--- back/src/api/github/login.py
+++ back/src/api/github/login.py
@@ -59,7 +59,7 @@ async def read_root(
         )
         if response.status_code == 200:
             parsed = parse_qs(response.content)
-            json_data = {key: values[0] for key, values in parsed.items()}
+            json_data = {str(key): str(values[0]) for key, values in parsed.items()}
             access_token = GithubAccessToken.model_validate(json_data)
         else:
             content = response.content
