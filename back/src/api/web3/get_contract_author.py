910c44ab743fed2f84e8c009f2be15db2057efde
aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>
2024-10-20 10:49:13 +0300

    Try another API method

--- back/src/api/web3/get_contract_author.py
+++ back/src/api/web3/get_contract_author.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Any
 
 from eth_typing import Address
 from fastapi import APIRouter
@@ -10,13 +11,15 @@ from config import settings
 contract_info_web3_router = APIRouter()
 
 
-def get_contact_creator(contract_address: str) -> str:
+def get_contact_creator(contract_address: str) -> str | Any:
     w3 = Web3(Web3.HTTPProvider(settings.RPC_URL))
 
     contract_address = Address(bytes.fromhex(contract_address[2:]))  # address str without 0x prefix
-    transaction = w3.eth.get_transaction_by_block(contract_address, 0)
-    creator_address = transaction['from']
-    return creator_address
+    transaction = w3.eth.get_transaction_receipt(contract_address, 0)
+    # transaction = w3.eth.get_transaction_by_block(contract_address, 0)
+    # creator_address = transaction['from']
+    # return creator_address
+    return transaction
 
 
 async def get_contact_creator_async(contract_address: str):
