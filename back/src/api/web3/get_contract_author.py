758336e7b4a3158c622902f7e17d67c232751630
aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>
2024-10-20 11:12:50 +0300

    Try with other endpoint

--- back/src/api/web3/get_contract_author.py
+++ back/src/api/web3/get_contract_author.py
@@ -1,30 +1,27 @@
-import asyncio
 from typing import Any
 
-from eth_typing import Address, HexStr
+import httpx
 from fastapi import APIRouter
 from pydantic import BaseModel
-from web3 import Web3
 
 from config import settings
 
 contract_info_web3_router = APIRouter()
 
 
-def get_contact_creator(contract_address: str) -> str | Any:
-    w3 = Web3(Web3.HTTPProvider(settings.RPC_URL))
+async def get_contact_creator(contract_address: str) -> Any:
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            f"{settings.RPC_URL}/getContractMetadata",
+            params={"contractAddress": contract_address},
+            headers={"accept": "application/json"}
+        )
 
-    # contract_address = Address(bytes.fromhex(contract_address[2:]))  # address str without 0x prefix
-    contract_address = HexStr(contract_address[2:])
-    transaction = w3.eth.get_transaction_receipt(contract_address)
-    # transaction = w3.eth.get_transaction_by_block(contract_address, 0)
-    # creator_address = transaction['from']
-    # return creator_address
-    return transaction
-
-
-async def get_contact_creator_async(contract_address: str):
-    return await asyncio.to_thread(get_contact_creator, (contract_address))
+        # Check if the request was successful
+        if response.status_code == 200:
+            contact_meta_data = response.json()
+            return contact_meta_data
+        raise RuntimeError("bad request from alchemy RPC_URL")
 
 
 class ContactRequest(BaseModel):
@@ -33,5 +30,5 @@ class ContactRequest(BaseModel):
 
 @contract_info_web3_router.post("/api/web3/contract-info")
 async def get_wallet_info(contract: ContactRequest):
-    creator = await get_contact_creator_async(contract.address)
+    creator = await get_contact_creator(contract.address)
     return {"creator": creator}
