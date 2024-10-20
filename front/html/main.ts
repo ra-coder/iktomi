e677072c502105e5976472ce37d97e011513d37e
aleksandr.ryabchenko <aleksandr.ryabchenko@mysky.com>
2024-10-20 14:25:46 +0300

    Try to add walletconnect

---
+++ front/html/main.ts
@@ -0,0 +1,41 @@
+// main.ts
+import { createAppKit } from '@reown/appkit'
+import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+
+// 1. Get a project ID at https://cloud.reown.com
+const projectId = '381e3b06914cf89bc18aa57f3816dec5'
+
+export const networks = [mainnet, arbitrum]
+
+// 2. Set up Wagmi adapter
+const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks
+})
+
+// 3. Configure the metadata
+const metadata = {
+  name: 'iktomi.pro',
+  description: 'AppKit Example',
+  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
+  icons: ['https://assets.reown.com/reown-profile-pic.png']
+}
+
+// 3. Create the modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  networks: [mainnet, arbitrum],
+  metadata,
+  projectId,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+// 4. Trigger modal programaticaly
+const openConnectModalBtn = document.getElementById('open-connect-modal')
+const openNetworkModalBtn = document.getElementById('open-network-modal')
+
+openConnectModalBtn.addEventListener('click', () => modal.open())
+openNetworkModalBtn.addEventListener('click', () => modal.open({ view: 'Networks' }))
